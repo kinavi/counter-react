daee55597195611f82063c118cef16cc12cd2683
vm-laptop <wolfmariev@gmail.com>
2020-02-03 16:29:00 +0500

    Редусер, счетчик, немного текстов

---
+++ src/redux/reducers.js
@@ -0,0 +1,33 @@
+// const initialState = {
+
+// };
+
+
+// {
+//     id,
+//     name,
+//     total-time
+//     time-left-for-lvl, (вычисляется на основе текущего времени)
+
+//     date-create,
+//     Races[{
+
+//     id
+//     last-date-start, (=дата создания )
+//     last-date-stop,
+//     limit (45 мин лимит)
+//     current-time,
+//     time-left,
+
+//     }]
+//     }
+
+//     Action:
+//     - add-tracke
+//     - remove-tracke
+//     - edit-tracke
+
+//     - start-count
+//     - stop-count
+
+// Subject -> Races
