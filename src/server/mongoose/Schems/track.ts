4dfb8d303cb07afdade5cf3029bb1bacdb1f5f03
v.mariev <v.mariev@raketa.travel>
2020-12-24 23:53:03 +0500

    using normalize-mongoose

--- src/server/mongoose/Schems/track.ts
+++ src/server/mongoose/Schems/track.ts
@@ -1,10 +1,11 @@
 import { SchemaDefinition } from 'mongoose';
+import normalize from 'normalize-mongoose';
 import { SchemaFactory } from '.';
 
 export class TrackSchema extends SchemaFactory {
   constructor(definition: SchemaDefinition) {
     super(definition);
-    this.schema.methods.getTracks = this.getTracks;
+    this.Schema.plugin(normalize);
   }
 
   // TODO: Надо разобраться с этой фигней
