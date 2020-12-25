f694307404a861f28779328145cd1006756de8c3
v.mariev <v.mariev@raketa.travel>
2020-12-26 00:50:00 +0500

    remove old code

--- src/old/components/UI/ButtonPlay.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {IconContext} from 'react-icons';
-import {FaPlay} from 'react-icons/fa';
-
-export const ButtonPlay = ({onClick}) =>
-  <IconContext.Provider value={{color: '#6F4D46'}}>
-    <FaPlay onClick={onClick} className='btn-timer'/>
-  </IconContext.Provider>;
--- src/old/components/UI/ButtonStop.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {IconContext} from 'react-icons';
-import {FaStop} from 'react-icons/fa';
-
-export const ButtonStop = ({onClick}) =>
-  <IconContext.Provider value={{color: '#6F4D46'}}>
-    <FaStop onClick={onClick} className='btn-timer'/>
-  </IconContext.Provider>;
