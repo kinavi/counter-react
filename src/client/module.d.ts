d811c8323882c475128a0b9da56febed062799b0
v.mariev <v.mariev@raketa.travel>
2021-01-09 15:47:17 +0500

    extend declare png, svg

--- src/client/module.d.ts
+++ src/client/module.d.ts
@@ -2,3 +2,13 @@ declare module 'react'
 declare module 'react-router-dom'
 declare module 'uuid'
 declare module 'classnames'
+
+declare module '*.png' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.svg' {
+    const content: any;
+    export default content;
+}
